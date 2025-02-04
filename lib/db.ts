0f4d061dba523cdfcdab78ead3564e53e11a69f8
Susmita-Dey <susmitadeywork@gmail.com>
2025-02-04 12:33:30 +0530

    add models and schema

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,44 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI!;
 
 if (!MONGODB_URI) {
-    throw new Error("Check your database connection string");
+  throw new Error("Check your database connection string");
 }
 
-// An assumption that we have a cached connection 
+// An assumption that we have a cached connection
 let cached = global.mongoose;
+
+// not cached means no connection
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+// connecting to database
+export async function connectToDatabase() {
+  // Case 1 : when we have the connection
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  // Case 2: when someone might have initiated the connection so promise is on the go
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: true,
+      maxPoolSize: 10,
+    };
+
+    cached.promise = mongoose
+      .connect(MONGODB_URI, opts)
+      .then(() => mongoose.connection);
+  }
+
+  // Case 3: When we don't have the promise
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+  }
+
+  return cached.conn; // Return the connection
+}
--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,11 @@
 import {Connection} from "mongoose";
 
+// global is a context available to the whole application 
 declare global{
     var mongoose:{
-        
+        conn: Connection | null;
+        promise: Promise<Connection>|null;
     }
 }
+
+export {};
